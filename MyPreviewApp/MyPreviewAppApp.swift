ef4693f57f0a95390aac0ba18aeef62d91319330
Valerie Don <valeriejdon@gmail.com>
2022-02-18 17:21:48 -0800

    Initial Commit

---
+++ MyPreviewApp/MyPreviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPreviewAppApp.swift
+//  MyPreviewApp
+//
+//  Created by Valerie Don on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MyPreviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
